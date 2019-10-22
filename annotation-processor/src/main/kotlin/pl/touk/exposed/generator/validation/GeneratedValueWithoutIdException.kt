b7bdd05b284691eb8f73586a588336e0952d653c
Piotr Jagielski <pjg@touk.pl>
2019-10-22 10:25:49 +0200

    Split EntityGraphBuilder to separate processor; @GeneratedValue processed with @Id in ColumnProcessor

--- annotation-processor/src/main/kotlin/pl/touk/exposed/generator/validation/GeneratedValueWithoutIdException.kt
+++
@@ -1,7 +0,0 @@
-package pl.touk.exposed.generator.validation
-
-import javax.lang.model.element.TypeElement
-import javax.lang.model.element.VariableElement
-
-class GeneratedValueWithoutIdException(varElement: VariableElement, entityType: TypeElement)
-    : RuntimeException("@GeneratedValue without @Id on field ${varElement.simpleName} in class ${entityType.simpleName}")
