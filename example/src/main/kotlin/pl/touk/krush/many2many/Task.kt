a8195c6fcfb55753daf2ed1dda05491eba3cd1f1
Namnodorel <felix.guenther@protonmail.com>
2020-06-21 11:41:30 +0200

    Added M2M self-reference test

---
+++ example/src/main/kotlin/pl/touk/krush/many2many/Task.kt
@@ -0,0 +1,17 @@
+package pl.touk.krush.many2many
+
+import javax.persistence.*
+
+@Entity
+@Table(name = "tasks")
+data class Task(
+
+        @Id @GeneratedValue
+        val id: Long? = null,
+
+        val description: String = "",
+
+        @JoinTable(name = "task_requirements")
+        @ManyToMany
+        val requirements: Collection<Task> = emptyList()
+)
---
+++ example/src/test/kotlin/pl/touk/krush/many2many/TaskTest.kt
@@ -0,0 +1,40 @@
+package pl.touk.krush.many2many
+
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.exposed.sql.Join
+import org.jetbrains.exposed.sql.JoinType
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.Test
+import pl.touk.krush.base.BaseDatabaseTest
+
+class TaskTest : BaseDatabaseTest() {
+
+    @Test
+    fun shouldHandleSelfReferences(){
+        transaction {
+            SchemaUtils.create(TaskTable, TaskRequirementsTable)
+
+            // given
+            val task1 = TaskTable.insert(Task(description = "Collect underpants"))
+            val task2 = TaskTable.insert(Task(description = "?", requirements = listOf(task1)))
+            val task3 = TaskTable.insert(Task(description = "Profit!", requirements = listOf(task1, task2)))
+
+            // when
+            val selectedTasks = Join(
+                    table = TaskTable,
+                    otherTable = TaskRequirementsTable,
+                    joinType = JoinType.LEFT,
+                    onColumn = TaskTable.id,
+                    otherColumn = TaskRequirementsTable.taskSourceId
+            ).selectAll().toTaskList()
+
+            // then
+            val expectation = listOf(task1, task2, task3)
+
+            assertThat(selectedTasks).isEqualTo(expectation)
+        }
+    }
+
+}
