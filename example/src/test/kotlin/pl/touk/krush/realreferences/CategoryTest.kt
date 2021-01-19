545ff2523ebc5351564cb64e441a4552801be1a6
Piotr Jagielski <pjg@touk.pl>
2021-01-19 11:51:52 +0100

    Self references generated code example

--- example/src/test/kotlin/pl/touk/krush/realreferences/CategoryTest.kt
+++ example/src/test/kotlin/pl/touk/krush/realreferences/CategoryTest.kt
@@ -1,6 +1,10 @@
 package pl.touk.krush.realreferences
 
+import org.assertj.core.api.Assertions.assertThat
+import org.jetbrains.exposed.sql.Alias
+import org.jetbrains.exposed.sql.Join
 import org.jetbrains.exposed.sql.JoinType
+import org.jetbrains.exposed.sql.ResultRow
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.alias
 import org.jetbrains.exposed.sql.selectAll
@@ -21,14 +25,60 @@ class CategoryTest: BaseDatabaseTest() {
             val child1 = CategoryTable.insert(Category(name = "child1", parent = parent))
             val child2 = CategoryTable.insert(Category(name = "child2", parent = parent))
 
-            CategoryTable.join(parentAlias, JoinType.LEFT, CategoryTable.parent, parentAlias[CategoryTable.id])
+            val categories =
+                CategoryTable.join(parentAlias, JoinType.LEFT, CategoryTable.parent, parentAlias[CategoryTable.id])
                 .selectAll()
-                .forEach { row ->
-                    val childName = row[CategoryTable.name]
-                    val parentName = row[parentAlias[CategoryTable.name]]
-                    println("$childName -> $parentName")
-                }
+                .map { it.toCategory(parentAlias) }
+
+            assertThat(categories)
+                .containsExactlyInAnyOrder(parent, child1, child2)
+
+            val fullCategories =
+                CategoryTable.join(parentAlias, JoinType.LEFT, CategoryTable.parent, parentAlias[CategoryTable.id])
+                    .selectAll()
+                    .toCategoryList(parentAlias)
+
+            println(fullCategories)
         }
     }
 
 }
+
+fun Iterable<ResultRow>.toCategoryList(parentAlias: Alias<CategoryTable>?): List<Category> =
+    this.toCategoryMap(parentAlias).values.toList()
+
+fun Iterable<ResultRow>.toCategoryMap(parentAlias: Alias<CategoryTable>?): MutableMap<Long, Category> {
+    val roots = mutableMapOf<Long, Category>()
+    val category_parent = mutableMapOf<Long, Category>()
+    val category_children = mutableMapOf<Long, MutableSet<Category>>()
+    this.forEach { resultRow ->
+        val categoryId = resultRow.getOrNull(CategoryTable.id) ?: return@forEach
+        val category = roots[categoryId] ?: resultRow.toCategory(parentAlias)
+        roots[categoryId] = category
+        resultRow[CategoryTable.parent]?.let { parentId ->
+            val parent = resultRow.toCategory(parentAlias)
+            category_parent[categoryId] = parent
+            category_children.getOrPut(parentId, ::mutableSetOf).add(category)
+        }
+    }
+    return roots.mapValues { (_, category) ->
+        category.copy(
+            parent = category_parent[category.id],
+            children = category_children[category.id]?.toList() ?: emptyList()
+        )
+    }.toMutableMap()
+}
+
+fun ResultRow.toCategory(parentAlias: Alias<CategoryTable>?): Category = Category(
+    id = this[CategoryTable.id],
+    name = this[CategoryTable.name],
+    parent = this[CategoryTable.parent]?.let { parentAlias?.let { this.toCategory(parentAlias, null) } },
+    children = mutableListOf()
+)
+
+fun ResultRow.toCategory(alias: Alias<CategoryTable>, parentAlias: Alias<CategoryTable>?): Category = Category(
+    id = this[alias[CategoryTable.id]],
+    name = this[alias[CategoryTable.name]],
+    parent = this[CategoryTable.parent]?.let { parentAlias?.let { this.toCategory(parentAlias, null) } },
+    children = mutableListOf()
+)
