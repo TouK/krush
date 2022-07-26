225e84e153bd4ac4222a62651f2c080574e55b14
Piotr Jagielski <pjg@touk.pl>
2022-07-26 09:53:41 +0200

    Add batchInsert and batchReplace examples

--- example/src/test/kotlin/pl/touk/krush/BookTest.kt
+++ example/src/test/kotlin/pl/touk/krush/BookTest.kt
@@ -1,18 +1,15 @@
 package pl.touk.krush
 
 import org.assertj.core.api.Assertions.assertThat
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.deleteAll
-import org.jetbrains.exposed.sql.replace
-import org.jetbrains.exposed.sql.select
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
-import org.jetbrains.exposed.sql.update
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import pl.touk.krush.base.BaseDatabaseTest
 import java.time.LocalDate
 import java.time.Month.APRIL
+import java.time.Month.AUGUST
+import java.time.Month.NOVEMBER
 
 class BookTest : BaseDatabaseTest() {
 
@@ -71,9 +68,8 @@ class BookTest : BaseDatabaseTest() {
             val updated = book.copy(publishDate = LocalDate.of(2021, APRIL, 11))
             BookTable.update({ BookTable.id eq bookId }, body = { it.from(updated) })
 
-            val selectedBooks = BookTable.selectAll().toBookList()
-
             // then
+            val selectedBooks = BookTable.selectAll().toBookList()
             assertThat(selectedBooks).containsOnly(updated)
         }
     }
@@ -98,15 +94,77 @@ class BookTest : BaseDatabaseTest() {
                 it[id] = bookId
                 it.from(book)
             }
-            BookTable.replace {
-                it[id] = bookId
-                it.from(book)
-            }
-
-            val selectedBooks = BookTable.selectAll().toBookList()
 
             // then
+            val selectedBooks = BookTable.selectAll().toBookList()
             assertThat(selectedBooks).containsOnly(book)
+        }
+    }
+
+    @Test
+    fun shouldBatchInsert() {
+        transaction {
+            SchemaUtils.create(BookTable)
+
+            // given
+            val got1 = Book(
+                isbn = "0-553-10354-7",
+                publishDate = LocalDate.of(1996, AUGUST, 1),
+                title = "A Game of Thrones",
+                author = "George R. R. Martin"
+            )
+
+            val got2 = Book(
+                isbn = "0-553-10803-4",
+                publishDate = LocalDate.of(1998, NOVEMBER, 16),
+                title = "A Clash of Kings",
+                author = "George R. R. Martin"
+            )
+
+            // when
+            BookTable.batchInsert(listOf(got1, got2), body = { book -> this.from(book) })
+
+            // then
+            val selectedBooks = BookTable.selectAll().toBookList()
+            assertThat(selectedBooks)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .containsExactly(got1, got2)
+        }
+    }
+
+    @Test
+    fun shouldBatchReplace() {
+        transaction {
+            SchemaUtils.create(BookTable)
+
+            // given
+            val got1 = Book(
+                isbn = "0-553-10354-7",
+                publishDate = LocalDate.of(1996, AUGUST, 1),
+                title = "A Game of Thrones",
+                author = "George R. R. Martin"
+            )
+
+            val got2 = Book(
+                isbn = "0-553-10803-4",
+                publishDate = LocalDate.of(1998, NOVEMBER, 16),
+                title = "A Clash of Kings",
+                author = "George R. R. Martin"
+            )
+
+            // when
+            BookTable.batchReplace(
+                listOf(got1 to 1, got2 to 2, got2 to 2),
+                body = { (book, id) ->
+                    this[BookTable.id] = id
+                    this.from(book)
+                })
+
+            // then
+            val selectedBooks = BookTable.selectAll().toBookList()
+            assertThat(selectedBooks)
+                .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
+                .containsExactly(got1, got2)
         }
     }
 }
