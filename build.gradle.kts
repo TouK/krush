8fae28263aa3ab84908c41f597b75e6be303f7f7
Piotr Jagielski <pjg@touk.pl>
2023-03-23 10:17:53 +0100

    Bump axion release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,14 +4,14 @@ plugins {
     kotlin("jvm") apply true
     kotlin("kapt") apply true
     kotlin("plugin.serialization") version "1.8.10"
-    id("pl.allegro.tech.build.axion-release") version "1.13.14"
+    id("pl.allegro.tech.build.axion-release") version "1.15.0"
     `maven-publish`
 }
 
 scmVersion {
-    useHighestVersion = true
+    useHighestVersion.set(true)
     tag {
-        prefix = "krush-"
+        prefix.set("krush-")
     }
 }
 
